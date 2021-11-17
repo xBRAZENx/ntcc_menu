9781a2c0548121f9854d8db0fce3c54ef0dc578f
xBRAZENx <87162097+xBRAZENx@users.noreply.github.com>
2021-11-17 15:44:20 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ function diplayMenuItems(menuItems) {
           <div class="item-info">
             <header>
               <h4>${item.title}</h4>
-              <h4 class="price">$${item.price}</h4>
+              <h4 class="price">₹${item.price}</h4>
             </header>
             <p class="item-text">
               ${item.desc}
